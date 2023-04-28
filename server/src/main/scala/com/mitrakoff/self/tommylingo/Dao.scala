0d1a8e3eb3f6ae58819c19e3916130b4566eba6b
mitrakov <mitrakov-artem@yandex.ru>
2023-04-28 13:41:37 +0300

    Connect to real DB

--- server/src/main/scala/com/mitrakoff/self/tommylingo/Dao.scala
+++ server/src/main/scala/com/mitrakoff/self/tommylingo/Dao.scala
@@ -16,16 +16,16 @@ CREATE TABLE dict (
 
 class Dao[F[_]](db: Db[F]):
   def fetchWithLimit(userId: Int, langCode: String, limit: Int): F[List[(String, String)]] =
-    db.run(sql"""SELECT key, translation FROM dict WHERE user_id = $userId AND lang_code = $langCode ORDER BY random() LIMIT $limit;""".query[(String, String)].to[List])
+    db.run(sql"""SELECT key, translation FROM lingo.dict WHERE user_id = $userId AND lang_code = $langCode ORDER BY random() LIMIT $limit;""".query[(String, String)].to[List])
 
   def fetchAllKeys(userId: Int, langCode: String): F[List[String]] =
-    db.run(sql"""SELECT key FROM dict WHERE user_id = $userId AND lang_code = $langCode;""".query[String].to[List])
+    db.run(sql"""SELECT key FROM lingo.dict WHERE user_id = $userId AND lang_code = $langCode;""".query[String].to[List])
 
   def persist(dict: Dict): F[Int] =
-    db.run(sql"""INSERT INTO dict (user_id, lang_code, key, translation)
+    db.run(sql"""INSERT INTO lingo.dict (user_id, lang_code, key, translation)
            VALUES (${dict.dictKey.userId}, ${dict.dictKey.langCode}, ${dict.dictKey.key}, ${dict.translation})
            ON CONFLICT (user_id, lang_code, key) DO UPDATE SET translation = ${dict.translation}
            ;""".update.run)
 
   def delete(dictKey: DictKey): F[Int] =
-    db.run(sql"""DELETE FROM dict WHERE user_id = ${dictKey.userId} AND lang_code = ${dictKey.langCode} AND key = ${dictKey.key};""".update.run)
+    db.run(sql"""DELETE FROM lingo.dict WHERE user_id = ${dictKey.userId} AND lang_code = ${dictKey.langCode} AND key = ${dictKey.key};""".update.run)
