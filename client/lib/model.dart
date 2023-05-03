55a490c9a5a8f9109da023f2d5532e7ea64c72e0
mitrakov <mitrakov-artem@yandex.ru>
2023-05-03 23:34:22 +0300

    MVP done

--- client/lib/model.dart
+++ client/lib/model.dart
@@ -1,5 +1,4 @@
-// ignore_for_file: curly_braces_in_flow_control_structures, avoid_print
-import 'dart:math';
+// ignore_for_file: curly_braces_in_flow_control_structures
 import 'package:scoped_model/scoped_model.dart';
 import 'package:tuple/tuple.dart';
 import 'package:http/http.dart' as http;
@@ -8,81 +7,35 @@ import 'package:xml/xml.dart';
 typedef TokenPair = Tuple2<String, String>;
 
 class MyModel extends Model {
-  // vals
-  final String _URL = "http://mitrakoff.com:9090/lingo";
-  final Set<String> _stopKeywords = {"RUS"};              // TODO
-  final Random _random = Random(DateTime.now().millisecondsSinceEpoch);
-  final Map<String, List<TokenPair>> _data = {}; // files -> tokens
-
-  // vars
-  String _currentFile = "";
+  final List<TokenPair> _data = [];
+  String _langCode = "en-GB";
   int _currentToken = 0;
 
-  // getters and setters
-  List<String> get files {
-    final list = _data.keys.toList();
-    list.sort();
-    return list;
-  }
-  TokenPair get token {
-    final list = _data[_currentFile] ?? [];
-    if (_currentToken >= list.length)
+  Future<TokenPair> get token async {
+    if (_currentToken >= _data.length) { // IF data is empty OR _currentToken is overflown
+      _data..clear()..addAll(await _loadMore());
       _currentToken = 0;
-    return list.isEmpty ? const Tuple2("", "") : list[_currentToken];
-  }
-  set currentFile(String s) {
-    _currentFile = s;
-    _currentToken = 0;
-    _data[s]?.shuffle(_random);
-    notifyListeners();
+    }
+    return _data.isEmpty ? const TokenPair("", "") : _data[_currentToken];
   }
 
-  // functions
   void nextToken() {
     _currentToken++;
     notifyListeners();
   }
 
-  void loadAll() async {
-    loadMore("en_US");
-    // final response = await http.get(Uri.parse(_URL));
-    // if (response.statusCode == 200) {
-    //   final htmlDoc = parse(response.body);
-    //   final elements = htmlDoc.getElementsByTagName("a");
-    //   for (final element in elements) {
-    //     _loadFile(element.text);
-    //   }
-    // } else throw Exception("Cannot load files from $_URL");
+  String get langCode => _langCode;
+
+  set langCode(String value) {
+    _langCode = value;
+    _data.clear();
+    notifyListeners();
   }
 
-  void _loadFile(String fileName) async {
-    final response = await http.get(Uri.parse("$_URL/$fileName"));
+  Future<Iterable<TokenPair>> _loadMore() async {
+    final response = await http.get(Uri.parse("http://mitrakoff.com:9090/lingo/translations/$langCode"));
     if (response.statusCode == 200) {
-      _data.putIfAbsent(fileName, () => []);
-      final body = response.body;
-      final lines = body.split("\n");
-      for (final line in lines) {
-        final tokens = line.split("|");
-        if (tokens.length > 2) {
-          final token1 = tokens[1].trim();
-          final token2 = tokens[2].trim();
-          if (token1.isNotEmpty && token2.isNotEmpty) {
-            if (!token1.contains("---") && !token2.contains("---")) {
-              if (!_stopKeywords.contains(token2)) {
-                _data[fileName]?.add(Tuple2(token1, token2));
-              }
-            }
-          }
-        }
-      }
-    } else throw Exception("Cannot load file $_URL/$fileName");
-  }
-
-  void loadMore(String langCode) async {
-    final response = await http.get(Uri.parse("$_URL/translations/$langCode"));
-    if (response.statusCode == 200) {
-      final all = XmlDocument.parse(response.body).findAllElements("item").map((e) => "${e.getAttribute("key")} -> ${e.text}");
-      print(all);
-    }
+      return XmlDocument.parse(response.body).findAllElements("item").map((e) => TokenPair(e.getAttribute("key")!, e.text));
+    } else return Future.error("Error: $response");
   }
 }
