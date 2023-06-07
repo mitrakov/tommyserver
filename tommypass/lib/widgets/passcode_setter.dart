d1525697a71f866c2ba470f3c845806c88a7b562
mitrakov <mitrakov-artem@yandex.ru>
2023-06-08 00:52:31 +0300

    Tommypass Client: refactor PasscodeSetter

--- tommypass/lib/widgets/passcode_setter.dart
+++ tommypass/lib/widgets/passcode_setter.dart
@@ -11,41 +11,30 @@ class PasscodeSetter extends StatelessWidget {
     return Scaffold(body: Center(child: Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.all(10),
-          child: TextField(
-            controller: ctrl1,
-            obscureText: true,
-            decoration: const InputDecoration(border: OutlineInputBorder(), labelText: "Set your passcode"),
-          )
+        TextField(
+          controller: ctrl1,
+          obscureText: true,
+          decoration: const InputDecoration(border: OutlineInputBorder(), labelText: "Set your passcode"),
         ),
-        Padding(
-          padding: const EdgeInsets.all(10),
-          child: TextField(
-            controller: ctrl2,
-            obscureText: true,
-            decoration: const InputDecoration(border: OutlineInputBorder(), labelText: "Verify your passcode"),
-          )
+        const SizedBox(height: 10),
+        TextField(
+          controller: ctrl2,
+          obscureText: true,
+          decoration: const InputDecoration(border: OutlineInputBorder(), labelText: "Verify your passcode"),
         ),
-        Padding(
-          padding: const EdgeInsets.all(40),
-          child: Builder(builder: (context1) => ElevatedButton(
-            child: const Text("Set passcode", style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600, fontSize: 18)),
-            onPressed: () {
-              if (ctrl1.text.isEmpty || ctrl2.text.isEmpty)
-                ScaffoldMessenger.of(context1).showSnackBar(const SnackBar(content: Text("Empty passcode!")));
-              else if (ctrl1.text == ctrl2.text)
-                setPasscode(ctrl1.text).then((b) => Navigator.popAndPushNamed(context1, "/main"));
-              else ScaffoldMessenger.of(context1).showSnackBar(const SnackBar(content: Text("Passcode fields differ!")));
-            },
-          )),
+        const SizedBox(height: 40),
+        ElevatedButton(
+          child: const Text("Set passcode", style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600, fontSize: 18)),
+          onPressed: () async {
+            if (ctrl1.text.isEmpty || ctrl2.text.isEmpty)
+              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Empty passcode!")));
+            else if (ctrl1.text == ctrl2.text) {
+              final storage = await SharedPreferences.getInstance();
+              storage.setString("passcode", ctrl1.text).then((value) => Navigator.popAndPushNamed(context, "/main"));
+            } else ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Passcode fields differ!")));
+          },
         ),
       ],
     )));
-  }
-
-  Future<bool> setPasscode(String passcode) async {
-    final storage = await SharedPreferences.getInstance();
-    return storage.setString("passcode", passcode);
   }
 }
