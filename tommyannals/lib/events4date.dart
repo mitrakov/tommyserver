cde0f8b8283faf03fe84be25deaa6cac8891e1b7
mitrakov <mitrakov-artem@yandex.ru>
2023-12-12 16:45:02 +0300

    Client: Tommyannals: add EventsForDate widget

---
+++ tommyannals/lib/events4date.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: use_key_in_widget_constructors
+import 'package:flutter/material.dart';
+
+class EventsForDateViewer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Hey-hey");
+  }
+}
