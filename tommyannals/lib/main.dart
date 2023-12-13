c055b48c1ed751c28e6dff86d00b28993919fe17
mitrakov <mitrakov-artem@yandex.ru>
2023-12-13 17:24:21 +0300

    Client: Annals: fix calendar

--- tommyannals/lib/main.dart
+++ tommyannals/lib/main.dart
@@ -1,11 +1,13 @@
 // ignore_for_file: use_key_in_widget_constructors
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:tommyannals/widgets/events4date.dart';
 import 'package:tommyannals/model.dart';
 
 void main() {
+  initializeDateFormatting(); // to load locales for TableCalendar
   runApp(ScopedModel(model: MyModel()..schema, child: MyApp()));
 }
 
@@ -18,9 +20,14 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(title: const Text("Tommy Annals")),
         body: Builder(builder: (context) { // wrap into a Builder to avoid "Navigator operation requested with a context" error
-          return TableCalendar(focusedDay: DateTime.now(), firstDay: DateTime.utc(2000), lastDay: DateTime.utc(2040), onDaySelected: (selectedDate, focusedDate) {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => EventsForDateViewer(selectedDate)));
-          });
+          return TableCalendar(
+            startingDayOfWeek: StartingDayOfWeek.monday,
+            locale: "es_ES", // make sure to call initializeDateFormatting() from import 'package:intl/date_symbol_data_local.dart';
+            focusedDay: DateTime.now(),
+            firstDay: DateTime.utc(2000),
+            lastDay: DateTime.utc(2040),
+            onDaySelected: (selectedDate, focusedDate) => Navigator.push(context, MaterialPageRoute(builder: (_) => EventsForDateViewer(selectedDate)))
+          );
         }),
       ),
     );
