1903b0ce08bcf17ca6ee2d3edd2261f54577a11d
Tommy-Air <tommy@air.ru>
2025-12-20 16:05:09 +0300

    Minor fixes

--- tommyannals/lib/widgets/events4date.dart
+++ tommyannals/lib/widgets/events4date.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: use_key_in_widget_constructors
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_alert/flutter_platform_alert.dart';
@@ -27,12 +26,14 @@ class EventsForDateViewer extends StatelessWidget {
 
   Widget _createTile(MyModel model, Chronicle item) {
     return TrixContainer(child: ListTile(
-      title: Text(item.eventName, textScaleFactor: 1.2),
-      subtitle: Text(json.encode(item.params), textScaleFactor: 0.85),
+      title: Text(item.eventName, textScaler: TextScaler.linear(1.2)),
+      subtitle: Text(json.encode(item.params), textScaler: TextScaler.linear(0.85)),
       onLongPress: () async {
-        const hdr = "Borrar evento";
-        final txt = "¿Estás seguro que quieres borrar: '${item.eventName}' (${item.date})?";
-        if (await FlutterPlatformAlert.showAlert(windowTitle: hdr, text: txt, alertStyle: AlertButtonStyle.yesNo, iconStyle: IconStyle.stop) == AlertButton.yesButton) {
+        if (await FlutterPlatformAlert.showAlert(
+            windowTitle: "Borrar evento",
+            text: "¿Estás seguro que quieres borrar: '${item.eventName}' (${item.date})?",
+            alertStyle: AlertButtonStyle.yesNo,
+            iconStyle: IconStyle.stop) == AlertButton.yesButton) {
           model.removeByChronicleId(date, item.id ?? 0);
         }
       },
