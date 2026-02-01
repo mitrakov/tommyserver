fdc14c87e4c5ac31852c216e3736558d8aac629a
tommy-air <mitrakov-artem@yandex.ru>
2026-02-01 15:26:04 +0300

    Server.Lingo. Add "soft-delete"

--- server/src/main/scala/com/mitrakoff/self/tommylingo/LingoDao.scala
+++ server/src/main/scala/com/mitrakoff/self/tommylingo/LingoDao.scala
@@ -10,6 +10,7 @@ CREATE TABLE lingo.dict (
  lang_code VARCHAR (8) NOT NULL,
  key VARCHAR (128) NOT NULL,
  translation VARCHAR (255) NOT NULL,
+ is_deleted BOOL NOT NULL DEFAULT false,
  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
  PRIMARY KEY (user_id, lang_code, key)
@@ -19,27 +20,22 @@ Conjugations: https://raw.githubusercontent.com/asosab/esp_verbos/master/esp_ver
 */
 
 class LingoDao[F[_]](db: Db[F]):
-  def fetch(userId: Int, langCode: String, key: String): F[Option[(String, String, Option[String])]] =
-    db.run(sql"""SELECT key, translation, conjugaciones FROM lingo.dict LEFT OUTER JOIN lingo.esp_verbos ON key = verbo WHERE user_id = $userId AND lang_code = $langCode AND key = $key"""
-      .query[(String, String, Option[String])].option)
+  def fetch(userId: Int, langCode: String, key: String): F[Option[(String, String, Boolean)]] =
+    db.run(sql"""SELECT key, translation, is_deleted FROM lingo.dict WHERE user_id = $userId AND lang_code = $langCode AND key = $key"""
+      .query[(String, String, Boolean)].option)
 
-  def fetch(userId: Int, langCode: String): F[List[(String, String, Option[String])]] =
-    db.run(sql"""SELECT key, translation, conjugaciones FROM lingo.dict LEFT OUTER JOIN lingo.esp_verbos ON key = verbo WHERE user_id = $userId AND lang_code = $langCode"""
-      .query[(String, String, Option[String])].to[List])
-
-  @deprecated
-  def fetchWithLimitAndRandom(userId: Int, langCode: String, limit: Int): F[List[(String, String)]] =
-    db.run(sql"""SELECT key, translation FROM lingo.dict WHERE user_id = $userId AND lang_code = $langCode ORDER BY random() LIMIT $limit;""".query[(String, String)].to[List])
-
-  @deprecated
-  def fetchAllKeys(userId: Int, langCode: String): F[List[String]] =
-    db.run(sql"""SELECT key FROM lingo.dict WHERE user_id = $userId AND lang_code = $langCode;""".query[String].to[List])
+  def fetch(userId: Int, langCode: String): F[List[(String, String, Boolean)]] =
+    db.run(sql"""SELECT key, translation, is_deleted FROM lingo.dict WHERE user_id = $userId AND lang_code = $langCode"""
+      .query[(String, String, Boolean)].to[List])
 
   def persist(dict: Dict): F[Int] =
     db.run(sql"""INSERT INTO lingo.dict (user_id, lang_code, key, translation)
            VALUES (${dict.dictKey.userId}, ${dict.dictKey.langCode}, ${dict.dictKey.key}, ${dict.translation})
            ON CONFLICT (user_id, lang_code, key) DO UPDATE SET translation = ${dict.translation}
            ;""".update.run)
+
+  def softDelete(dictKey: DictKey, delete: Boolean = true): F[Int] =
+    db.run(sql"""UPDATE lingo.dict SET is_deleted = $delete WHERE user_id = ${dictKey.userId} AND lang_code = ${dictKey.langCode} AND key = ${dictKey.key};""".update.run)
 
   def delete(dictKey: DictKey): F[Int] =
     db.run(sql"""DELETE FROM lingo.dict WHERE user_id = ${dictKey.userId} AND lang_code = ${dictKey.langCode} AND key = ${dictKey.key};""".update.run)
--- server/src/main/scala/com/mitrakoff/self/tommylingo/LingoService.scala
+++ server/src/main/scala/com/mitrakoff/self/tommylingo/LingoService.scala
@@ -1,10 +1,12 @@
 package com.mitrakoff.self.tommylingo
 
 class LingoService[F[_]](dao: LingoDao[F]):
-  def getByKey(userId: Int, langCode: String, key: String): F[Option[(String, String, Option[String])]] = dao.fetch(userId, langCode, key)
+  def getByKey(userId: Int, langCode: String, key: String): F[Option[(String, String, Boolean)]] = dao.fetch(userId, langCode, key)
 
-  def getAll(userId: Int, langCode: String): F[List[(String, String, Option[String])]] = dao.fetch(userId, langCode)
+  def getAll(userId: Int, langCode: String): F[List[(String, String, Boolean)]] = dao.fetch(userId, langCode)
 
   def upsert(dict: Dict): F[Int] = dao.persist(dict)
+
+  def softDelete(dictKey: DictKey): F[Int] = dao.softDelete(dictKey)
 
   def remove(dictKey: DictKey): F[Int] = dao.delete(dictKey)
