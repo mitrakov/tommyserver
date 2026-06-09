d406b419405383421b34f152b1ef92b35b7543bf
tommy <mitrakov-artem@yandex.ru>
2026-06-09 18:26:34 +0300

    Kcal Client: add colours

--- tommykcal/lib/widgets/meal4date.dart
+++ tommykcal/lib/widgets/meal4date.dart
@@ -19,12 +19,21 @@ class MealForDateViewer extends StatelessWidget {
         if (snapshot.hasError) return Text("ERROR: ${snapshot.error}");
         if (!snapshot.hasData) return Text("Ningún dato para: $date");
         final list = snapshot.data!;
+        final total = list.map((meal) => meal.kcalTotal).sum;
         return Column(
           crossAxisAlignment: .center,
           spacing: 10,
           children: [
             const Text("La comida", style: TextStyle(fontWeight: .bold, fontSize: 20)),
-            Text("En total: ${list.map((meal) => meal.kcalTotal).sum} kcal"),
+            Row(
+              mainAxisAlignment: .center,
+              spacing: 10,
+              children: [
+                Text("En total: $total kcal"),
+                Container(height: 20, width: total * 0.07, decoration: 
+                  BoxDecoration(color: _kcal2colour(total), border: .all(color: Colors.grey))),
+              ],
+            ),
             Expanded(child: ListView(children: list.map((meal) => _createTile(model, meal)).toList())),
           ],
         );
@@ -48,5 +57,18 @@ class MealForDateViewer extends StatelessWidget {
         }
       },
     ));
+  }
+  
+  Color _kcal2colour(int kcal) {
+    if (kcal < 1600) return Colors.green;
+    if (kcal < 1700) return Colors.lightGreen;
+    if (kcal < 1800) return Colors.lightGreenAccent;
+    if (kcal < 1900) return Colors.yellow;
+    if (kcal < 2000) return Colors.yellowAccent;
+    if (kcal < 2100) return Colors.orangeAccent;
+    if (kcal < 2200) return Colors.orange;
+    if (kcal < 2300) return Colors.deepOrangeAccent;
+    if (kcal < 2400) return Colors.deepOrange;
+    return Colors.red;
   }
 }
